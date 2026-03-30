8298387f4a9535cdf06a2467e51b91604913ffee
Rasikh <rrasikh@offis.de>
2026-03-30 12:48:01 +0200

    Master Fix for Server Error

--- Rayhancollection.py
+++ Rayhancollection.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    # This looks for your 'templates' folder
     return render_template('index.html')
 
 if __name__ == "__main__":
+    # This is the "Magic Key" for Render
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
